7f457a25c0e0ea3359a78ae0ef26981cbfa7af65
Sememok <134376522+Sememok@users.noreply.github.com>
2026-01-08 01:33:33 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+const RANKS = [
+  {
+    id: "pplus",
+    title: "Partner Plus",
+    youtube: "M293NdN7Sok"
+  },
+  {
+    id: "sp",
+    title: "Senior Partner",
+    youtube: "fnG6Eld0SPk"
+  },
+  {
+    id: "sc",
+    title: "Sales Coordinator",
+    youtube: "cyJb_ecWjyA"
+  },
+  {
+    id: "qssc",
+    title: "Qualifying Senior Sales Coordinator",
+    youtube: "ru06kEc9kqE"
+  },
+  {
+    id: "ssc",
+    title: "Senior Sales Coordinator",
+    youtube: "xPwmFecZ8Ms"
+  },
+  {
+    id: "qnmd",
+    title: "Qualifying National Marketing Director",
+    youtube: "W8Wm_c4kMUo"
+  },
+  {
+    id: "nmd",
+    title: "National Marketing Director",
+    youtube: "muieHSXIocI"
+  },
+  {
+    id: "imd",
+    title: "International Marketing Director",
+    youtube: "WeEZlCjHAtU"
+  },
+  {
+    id: "emd",
+    title: "Executive Marketing Director",
+    youtube: "KpKDNIc8R7k"
+  },
+  {
+    id: "pmd",
+    title: "Presidential Marketing Director",
+    youtube: "2i3sVTpFxts"
+  },
+  {
+    id: "pmdplus",
+    title: "Presidential Marketing Director Plus",
+    youtube: "MVxQ4LPsj6w"
+  }
+];
